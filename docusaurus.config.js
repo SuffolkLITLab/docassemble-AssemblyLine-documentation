6d5b35a4ffcf825698cb2f2826b99e448e453ffe
samglover <sam@samglover.net>
2024-07-11 20:03:45 -0500

    Indentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -138,10 +138,11 @@ module.exports = {
   ],
   themes: [
 	  [
-	  require.resolve("@easyops-cn/docusaurus-search-local"),
-	  {
-		  hashed: true,
-	  }],
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+      }
+    ],
   ],
   presets: [
     [
