58d354e482bb42392301e3d20c7e9f32c59ede15
plocket <52798256+plocket@users.noreply.github.com>
2026-03-31 10:33:35 -0400

    Fix volunteer signup link (#600)

--- docs/react_components/VolunteerFormLink.jsx
+++ docs/react_components/VolunteerFormLink.jsx
@@ -16,7 +16,7 @@ export function VolunteerFormLink( props ) {
     sign_up_text = `Sign up to volunteer`
   }
 
-  return (<div><button
+  return (<div><a
     className={`button button--primary ${ props.className }`}
     target="_blank"
     // disabled={ props.disabled }
@@ -30,7 +30,7 @@ export function VolunteerFormLink( props ) {
     }>
       { sign_up_text }
     <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
-    </button></div>
+    </a></div>
   )
 
 };
